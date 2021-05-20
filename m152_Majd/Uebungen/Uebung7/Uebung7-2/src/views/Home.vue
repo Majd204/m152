3aeaf787e4fcf501d86d6c39323fc33035583d73
Majd Amro <Majd_Amro@sluz.ch>
2021-05-20 19:34:40 +0200

    Uebung 7

---
+++ m152_Majd/Uebungen/Uebung7/Uebung7-2/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="content">
+
+  <ul>
+  <li v-for="todo in todos">{{todo}} </li>
+  </ul>
+  
+  <input v-model="newTodo"/>
+  <button v-on:click="addTodo()">Hinzufügen</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function(){
+    return{
+
+      todos:[ 'Helloworld',
+              'bibabu',
+              'wasus'
+
+  
+      ],
+      newTodo:''
+    }
+  },
+  methods:{
+    addTodo: function(){
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.content{
+width: 100px;
+margin: 0 auto;
+}
+</style>
---
+++ m152_Majd/Uebungen/Uebung7/Uebung7-3/src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="content">
+   <ul>
+  <li v-for="noten in noten">{{noten.name, noten.grades}} </li>
+  
+  </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function(){
+    return{
+
+
+  noten:[
+
+    {
+        name: "Mathematik",
+        grades: [5,4.7,3.5,4.1] 
+    },
+    {
+        name: "Geschichte",
+        grades: [5.5,4.9,5.75] 
+    },
+    {
+        name: "Physik",
+        grades: [2,2.7,4.3] 
+    },
+    {
+        name: "Deutsch",
+        grades: [4.25,4.1,3.5] 
+    },
+]
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.content{
+width: 100px;
+margin: 0 auto;
+} 
+</style>
